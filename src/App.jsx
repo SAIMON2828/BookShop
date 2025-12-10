7c5f681bc1c8814d5eb56e9adc4649e6bcfbb82b
saimon2828 <mdsaimon28285@gmail.com>
2025-12-10 23:09:38 +0600

    add books

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Navbar from './Components/NavBar/Navbar'
+
+function App() {
+
+
+  const [bookmarked, setBookmarked] = useState([]);
+  const [readingCount, setReadingCount] = useState(0);
+
+  const handleBookMark = (blog) =>{
+    setBookmarked([...bookmarked, blog])
+  }
+  
+
+  const handleMarkAsRead = (time,id)=>{
+    setReadingCount(readingCount + time);
+    handleRemoveFromBookMark(id)
+  }
+
+  const handleRemoveFromBookMark = (id)=>{
+    const remainingBookMark = bookmarked.filter((mark)=> mark.id !== id);
+    setBookmarked(remainingBookMark)
+  }
+
+  return (
+    <>
+      
+      <Navbar></Navbar>
+      
+
+      <div className="main-container flex text-center">
+        <div className="left-container w-[70%]">
+          
+
+          <Blogs handleBookMark={handleBookMark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+        </div>
+        <div className="right-container w-[30%]">
+          <h1>Reading time : {readingCount}</h1>
+          <h1>Bookmarkred count: {bookmarked.length}</h1>
+
+          {
+            bookmarked.map((marked) =><p key={marked.id} className='bg-amber-700 p-2 shadow-amber-500 m-4 rounded-lg'>{marked.title}</p>)
+          }
+        </div>
+      </div>
+      
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import Blog from '../Blog/Blog';
+
+const Blogs = ({handleBookMark, handleMarkAsRead}) => {
+
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(()=>{
+        fetch("Blog.json")
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    }, [])
+
+// console.log(blogs)
+
+    return (
+        <div>
+            <div>
+                <h1 className='text-3xl'>Total : {blogs.length}</h1>
+                <div className="all-blogs grid grid-cols-2">
+                    {
+                        blogs.map((blog)=> <Blog key={blog.id} blog={blog} handleBookMark={handleBookMark} handleMarkAsRead={handleMarkAsRead}></Blog>)
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Blogs;
